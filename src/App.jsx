f4b1b4ad7b6b8027ad699e62af60d6385f985324
simon-bertram <simonbertram@hotmail.com>
2023-11-03 18:15:29 +0000

    Intital project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-red-500 text-3xl underline">
+          Mern Estate
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
