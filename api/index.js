981bbffd2b20064ec46a4117094f4dad3c75f9a2
simon-bertram <simonbertram@hotmail.com>
2023-11-03 22:59:20 +0000

    Setup mongoose and dotenv and connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('Connected to MongoDB');
+  }).catch((err) => {
+    console.log('Error connecting to MongoDB', err.message);
+});
+  
 
 const app = express();
 
