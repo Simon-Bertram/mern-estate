66af6af18dd2d6fd9460c2d8850ba6dee8c03b60
simon-bertram <simonbertram@hotmail.com>
2023-11-04 09:24:18 +0000

    Add User model

---
+++ api/models/user.model.js
@@ -0,0 +1,33 @@
+import { time } from 'console';
+import mongoose from 'mongoose';
+import { timestamp } from 'rxjs';
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 20,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    maxlength: 64,
+    trim: true
+  }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
