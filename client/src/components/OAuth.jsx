66ef8057d9b167e25fc642919d661e61db6e0e56
simon-bertram <simonbertram@hotmail.com>
2023-11-07 15:49:40 +0000

    Complete OAuth component with fetch to backend

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,7 +1,11 @@
 import { GoogleAuthProvider, getAuth, signInWithPopup } from '@firebase/auth';
 import { app } from '../firebase';
+import { useDispatch } from 'react-redux';
+import { signInSuccess } from '../redux/user/userSlice';
 
 const OAuth = () => {
+  const dispatch = useDispatch();
+
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
@@ -9,7 +13,17 @@ const OAuth = () => {
 
       const result = await signInWithPopup(auth, provider);
       
-      console.log(result);
+      const res = await fetch('/api/auth/google', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name: result.user.displayName, email: result.user.email, photo: result.user.photoURL}),
+      });
+      const data = await res.json();
+      console.log(data);
+      dispatch(signInSuccess(data));
+
     } catch (error) {
       console.log('Could not sign in with Google', error.message);
     }
