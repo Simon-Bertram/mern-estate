6069b0d9564cc0ee55f061e944129dd1607431dd
simon-bertram <simonbertram@hotmail.com>
2023-11-07 15:33:22 +0000

    Complete OAuth component

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,14 +1,26 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from '@firebase/auth';
+import { app } from '../firebase';
+
 const OAuth = () => {
   const handleGoogleClick = async () => {
     try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
+
+      const result = await signInWithPopup(auth, provider);
       
+      console.log(result);
     } catch (error) {
       console.log('Could not sign in with Google', error.message);
     }
   };
 
   return ( 
-    <button type="button" className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-80 w-full mt-3">
+    <button 
+      onClick={handleGoogleClick}
+      type="button" 
+      className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-80 w-full mt-3"
+    >
       Continue with Google
     </button>
    );
