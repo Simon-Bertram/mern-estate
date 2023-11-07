1d96fcd342f1b5afa9f8391979b8f829e1feab3b
simon-bertram <simonbertram@hotmail.com>
2023-11-07 15:27:19 +0000

    Install firebase and create firebase.js config file, putting api key in .env

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,4 +1,12 @@
 const OAuth = () => {
+  const handleGoogleClick = async () => {
+    try {
+      
+    } catch (error) {
+      console.log('Could not sign in with Google', error.message);
+    }
+  };
+
   return ( 
     <button type="button" className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-80 w-full mt-3">
       Continue with Google
